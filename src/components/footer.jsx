4d96c7ffea8e58e062fba1535e430936d45ac381
peter <solomon9964@gmail.com>
2023-08-24 12:47:11 +0530

    updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,9 @@ const footer = () => {
 
   return (
     <div className=" flex  bg-red-600 items-center justify-center py-5 mt-10">
-      <div className=" text-xl text-white">&copy; {currentYear} Restaurant</div>
+      <div className=" text-xl text-white">
+        &copy; {currentYear} Restaurant . All right reseverd
+      </div>
     </div>
   );
 };
